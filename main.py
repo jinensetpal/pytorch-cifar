10a66a8228875dd0570ae6ef023b7d35479f4ec9
Jinen Setpal <jinens8@gmail.com>
2024-08-25 11:11:38 -0400

    modified dataset to oxford-iiit-pets

--- main.py
+++ main.py
@@ -14,9 +14,12 @@ import argparse
 from models import *
 from utils import progress_bar
 
+from src.data.oxford_iiit_pet import get_generators
 from src.model.loss import ContrastiveLoss
 from src.model.arch import Model
 from src import const
+
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
@@ -42,18 +45,17 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+# trainset = torchvision.datasets.CIFAR10(
+#     root='./data', train=True, download=True, transform=transform_train)
+# trainloader = torch.utils.data.DataLoader(
+#     trainset, batch_size=128, shuffle=True, num_workers=2)
+#
+# testset = torchvision.datasets.CIFAR10(
+#     root='./data', train=False, download=True, transform=transform_test)
+# testloader = torch.utils.data.DataLoader(
+#     testset, batch_size=100, shuffle=False, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+trainloader, testloader, _ = get_generators()
 
 # Model
 print('==> Building model..')
@@ -71,7 +73,9 @@ print('==> Building model..')
 # net = ShuffleNetV2(1)
 # net = EfficientNetB0()
 # net = RegNetX_200MF()
-const.N_CLASSES = 10
+const.BASE_DIR = Path(__file__).parent.parent / 'contrastive-optimization'
+print(const.BASE_DIR)
+const.N_CLASSES = 2
 net = Model(const.IMAGE_SHAPE, is_contrastive=False)
 # net = net.to(device)
 if device == 'cuda':
@@ -93,7 +97,7 @@ optimizer = optim.SGD(net.parameters(), lr=args.lr,
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
 def ohe(tensor):
-    y = torch.zeros(tensor.shape[0], len(classes))
+    y = torch.zeros(tensor.shape[0], const.N_CLASSES)
     for idx, class_idx in enumerate(tensor.tolist()): y[idx, class_idx] = 1.
     return y
 
@@ -105,17 +109,16 @@ def train(epoch):
     correct = 0
     total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
-        inputs, targets = inputs.to(device), ohe(targets).to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
-        loss = criterion(outputs[0], targets)
+        loss = criterion(outputs[0], targets[1])
         loss.backward()
         optimizer.step()
 
         train_loss += loss.item()
         predicted = outputs[0].argmax(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets.argmax(1)).sum().item()
+        total += targets[1].size(0)
+        correct += predicted.eq(targets[1].argmax(1)).sum().item()
 
         progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
@@ -123,20 +126,18 @@ def train(epoch):
 
 def test(epoch):
     global best_acc
-    net.eval()
     test_loss = 0
     correct = 0
     total = 0
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
-            inputs, targets = inputs.to(device), ohe(targets).to(device)
             outputs = net(inputs)
-            loss = criterion(outputs[0], targets)
+            loss = criterion(outputs[0], targets[1])
 
             test_loss += loss.item()
             predicted = outputs[0].argmax(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets.argmax(1)).sum().item()
+            total += targets[1].size(0)
+            correct += predicted.eq(targets[1].argmax(1)).sum().item()
 
             progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
